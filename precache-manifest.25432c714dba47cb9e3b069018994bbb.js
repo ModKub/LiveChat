f594d99ef97b0cc38d810ebea6906f1887ec430b
ModKub <JakubModzelewski@gogs.storwork.pl>
2019-05-15 10:57:05 +0200

    Updates

---
+++ precache-manifest.25432c714dba47cb9e3b069018994bbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28da8cf5338a807a38caa017cc81175f",
+    "url": "/LiveChat/index.html"
+  },
+  {
+    "revision": "e685f9090437e5a2ed9e",
+    "url": "/LiveChat/static/css/main.a86cfec6.chunk.css"
+  },
+  {
+    "revision": "86235c4a402f89d6f03e",
+    "url": "/LiveChat/static/js/2.69601102.chunk.js"
+  },
+  {
+    "revision": "e685f9090437e5a2ed9e",
+    "url": "/LiveChat/static/js/main.3e4f98d5.chunk.js"
+  },
+  {
+    "revision": "d35aadfa1a0ee672f9e8",
+    "url": "/LiveChat/static/js/runtime~main.5f4aefe9.js"
+  },
+  {
+    "revision": "170faf7e7a838261d41cc6a29f588ed6",
+    "url": "/LiveChat/static/media/avatar.170faf7e.png"
+  }
+]);
